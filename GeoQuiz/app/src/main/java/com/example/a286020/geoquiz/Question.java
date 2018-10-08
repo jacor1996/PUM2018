448917bad4cbcd70bfb3d1b303524880810ce953
Jacek Noga <jaceknoga11@gmail.com>
2018-10-08 19:28:52 +0200

    Initial commit

---
+++ GeoQuiz/app/src/main/java/com/example/a286020/geoquiz/Question.java
@@ -0,0 +1,6 @@
+package com.example.a286020.geoquiz;
+
+public class Question {
+    private int mTextResId;
+    private boolean 
+}
---
+++ GeoQuiz/app/src/main/java/com/example/a286020/geoquiz/QuizActivity.java
@@ -0,0 +1,36 @@
+package com.example.a286020.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class QuizActivity extends AppCompatActivity {
+    private Button falseButton;
+    private Button trueButton;
+    private View textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        falseButton = (Button)findViewById(R.id.falseButton);
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        trueButton = (Button)findViewById(R.id.trueButton);
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
